c33e7bc1963d3f40274213e8bb13fa5e203cad54
SongMinJi <minjirj@naver.com>
2021-11-19 21:59:21 +0900

    '수정기능'수정

--- routers/board.js
+++ routers/board.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.get("/board/:boardId", async (req, res) => {
   const { boardId } = req.params;
   board = await Board.findById(boardId);
-  
+
   /**
    * 내 생각에는 모든 도큐먼트의 보드아이디가 전부 하나로 통일되어 있음
    * findbyid로 개별 id 가져옴
-   * 
+   *
    */
   res.json({ detail: board });
 });
@@ -20,7 +20,7 @@ router.get("/board/:boardId", async (req, res) => {
 router.get("/board", async (req, res, next) => {
   try {
     const { boardId } = req.query;
-    
+
     const board = await Board.find({ boardId }).sort("-date");
     res.json({ board: board });
   } catch (err) {
@@ -29,26 +29,46 @@ router.get("/board", async (req, res, next) => {
   }
 });
 
-
 //저장
-router.post('/board', async (req, res) => {
+router.post("/board", async (req, res) => {
   const { title, author, password, contents, date } = req.body;
   isExist = await Board.find({ title });
   if (isExist.length == 0) {
-    await Board.create({title, author, password, contents, date });
+    await Board.create({ title, author, password, contents, date });
   }
   res.send({ result: "success" });
 });
 
 //수정
-router.patch("/board/:/boardId", async (req, res) => {
+router.patch("/board/:boardId", async (req, res) => {
   const { boardId } = req.params;
-  const {title, author, contents } = req.body;
-  isBoard = await Board.find({ boardId });
-  if (isBoard.length) {
-    await Board.updateOne({ boardId }, { $set: { title, author, contents } });
+  const { title, author, contents, password } = req.body;
+
+  //board = await Board.updateOne({ boardId }); // 여기가 문제 => 날리니 괜찮은데 다른게 튀어나오네..?
+
+  if (board.password === password) {
+    await Board.updateOne(
+      { _id: boardId }, // 여기 수정했는데 왜지..
+      { $set: { title, author, contents } }
+    );
+    console.log(board);
+    res.send({ result: "success" });
+  } else {
+    res.send({ result: "fail" });
   }
-  res.send({ result: "success" });
-})
+});
+
+//삭제
+router.delete("/board/:boardId", async (req, res) => {
+  const { boardId } = req.params;
+  const board = await Board.find({ boardId });
+  //board = await Board.updateOne({ boardId }); // 여기가 문제 날리니 괜찮은데 다른게 튀어나오네..?
+
+  if (board.length > 0) {
+    await Board.deleteOne({ _id: boardId });
+
+    res.send({ result: "success" });
+  }
+});
 
 module.exports = router;
